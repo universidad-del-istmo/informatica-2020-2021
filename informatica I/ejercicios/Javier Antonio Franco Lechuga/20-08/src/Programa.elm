b6712ea3d6bf3afb5bca632e1fc2b457f9e20a69
Javier Franco <jafranco@unis.edu.gt>
2020-08-20 23:42:13 -0600

    Laboratorio 2

---
+++ informatica I/ejercicios/Javier Antonio Franco Lechuga/20-08/src/Programa.elm
@@ -0,0 +1,20 @@
+module Programa exposing (..)
+
+-- Retorna una lista de tal forma que v1,v2,3
+-- esten ordenados en la lista resultante
+ordenar v1 v2 = if (if v1 < v2 > v3 then True else False)  then (v1, v2, v3) else (v2, v1, v3)
+
+-- Si las 4 coordenadas forman un cuadrado,
+-- retornar el area dentro de un just.
+-- De lo contrario, retornar nothing
+areaMaybe (x1,y1) (x2,y2) (x3,y3) (x4,y4) = "?"
+x1y1 
+punto1 = (x1y1)
+
+
+-- Suma el area de los dos cuadrados
+--- Acepta dos cuadrados como los que utiliza
+-- areaMaybe y suma sus areas.
+-- Si ninguno es cuadrado, retornar 0
+-- Si solo uno es cuadrado, retornar el area de ese cuadrado
+sumarAreas  = "?"
