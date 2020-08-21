ed16c86b7d8f7b7dd716865f2a00087e1d2eec15
Rogchiu <rmazariegos@unis.edu.gt>
2020-08-21 13:19:30 -0600

    Ejercicio 13-08 y 20-08

---
+++ informatica I/Ejercicios/20-08/src/Programa.elm
@@ -0,0 +1,18 @@
+module Programa exposing (..)
+
+
+
+--> List.sort [4,2,6]
+--[2,4,6] : List number
+
+--cuadrado = [0,0][1,0][0,1][1,1]
+--areaMayby = if cuadrado = cuadrado then 1 * 1 else Nothing
+
+
+
+
+
+
+
+
+
