366cf1f8dd28614ce80371ebd60155afb9e8a482
claupons23 <claudettepbosch@gmail.com>
2020-08-25 10:24:30 -0600

    ejercicio 25-08

--- informatica I/ejercicios/Claudette Pons/25-08/src/Programa.elm
+++ informatica I/ejercicios/Claudette Pons/25-08/src/Programa.elm
@@ -16,4 +16,4 @@ s2 = "23"
 -- Calcular la suma de s1 + s2 utilizando
 -- la funcion String.toInt para convertir los
 -- strings a numeros enteros
-sumaDeStrings = Maybe.withDefault 0 (String.toInt "456") + Maybe.withDefault 0 (String.toInt "23")
+sumaDeStrings = Maybe.withDefault 0 (String.toInt s1) + Maybe.withDefault 0 (String.toInt s2)
