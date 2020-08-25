b8215405e337956539e9b23cb2e5ad5b3ffe4103
Rogchiu <rmazariegos@unis.edu.gt>
2020-08-25 09:31:28 -0600

    ejercicio 25-08

---
+++ informatica I/Ejercicios/25-08/src/Programa.elm
@@ -0,0 +1,19 @@
+module Programa exposing 
+
+
+v1 = List.head [1,2,3]
+
+v2 = List.head [4,5,6]
+
+v3 = List.head []
+
+v4 = Maybe.withDefault 0 v1 + Maybe.withDefault 0 v3 
+
+s1 = "456"
+
+s2 = "23"
+
+--Ejercicio en clase 
+sumaDeStrings = Maybe.withDefault 0 (String.toInt s1) + Maybe.withDefault 0 (String.toInt s2)
+
+
