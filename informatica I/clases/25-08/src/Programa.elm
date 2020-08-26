4ce1eec5ccf3b7033a6d92c9a608f65dd0f81b20
jrosado129 <jrosado@unis.edu.gt>
2020-08-25 19:01:04 -0600

    Ejercicio 25 08 Jorge Rosado

--- informatica I/clases/25-08/src/Programa.elm
+++ informatica I/clases/25-08/src/Programa.elm
@@ -16,7 +16,8 @@ s2 = "23"
 -- Calcular la suma de s1 + s2 utilizando
 -- la funcion String.toInt para convertir los
 -- strings a numeros enteros
-sumaDeStrings = ?
+sumaDeStrings = Maybe.withDefault 0 (String.toInt s1) + Maybe.withDefault 0 (String.toInt s2)
+
 
 -- sumarHastaN 5 = 1 + 2 + 3 + 4 + 5
 -- sumarHastN 3 = 1 + 2 + 3
