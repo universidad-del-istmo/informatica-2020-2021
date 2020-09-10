2739a6020b0e42e8f459aec05c4f9307476ef878
Jose Ortiz <jose1201ortiz@gmail.com>
2020-09-10 08:26:08 -0600

    practica lab

--- informatica I/ejercicios/JoseOrtiz/25-08/src/Programa.elm
+++ informatica I/ejercicios/JoseOrtiz/25-08/src/Programa.elm
@@ -7,3 +7,14 @@ s1Caracter = String.toInt s1
 s2Caracter = String.toInt s2
 
 sumaDeStrings= Maybe.withDefault 0 s1Caracter + Maybe.withDefault 0 s2Caracter
+
+
+parImpar n = 
+    if remainderBy 2 n == 0 
+    then "par"
+    else "impar"
+
+existe m x = 
+    if x < List.head m || x > List.tail m 
+    then "false" 
+    else "true"
