1b4fbe13ca6812c3c1670299141dd4a433c62678
sachiko444 <hgabrie@unis.edu.gt>
2020-09-11 11:47:43 -0600

    ejercicio Minimo y Maximo

--- informatica I/ejercicios/Sofia Gabrie/08-09/src/MinMax.elm
+++ informatica I/ejercicios/Sofia Gabrie/08-09/src/MinMax.elm
@@ -1,4 +1,8 @@
-module Programa2 exposing (..)
+module MinMax exposing (..)
+
+
+
+--VALOR MAXIMO EN LISTA
 
 maximoAux xs estado =
     case xs of
@@ -8,41 +12,13 @@ maximoAux xs estado =
         then maximoAux bs b
         else maximoAux bs estado
 
--- maximoAux [2,1] 0
--- case [2,1] of
--- [] -> estado
--- b :: bs -> if b > estado then maximoAux bs b else maximoAux bs estado
-
--- if 2 > 0 then maximoAux [1] 2 else maximoAux [1] 0
--- maximoAux [1] 2
-
--- case [1] of
--- [] -> estado
--- b :: bs -> if b > estado then maximoAux bs b else maximoAux bs estado
-
--- if 1 > 2 then maximoAux [] 1 else maximoAux [] 2
--- maximoAux [] 2
-
--- case [] of
--- [] -> estado
--- b :: bs -> ...
-
--- estado
--- = 2
-
 maximo xs =
     case xs of
     [] -> Nothing
     b1 :: bs ->
         Just (maximoAux bs b1)
 
--- maximo [0,2,1]
--- case [0,2,1] of
--- [] -> Nothing
--- b1 :: bs -> Just (maximoAux bs b1)
-
--- Just (maximoAux [2,1] 0)
--- = Just 2
+--ANOTHER WAY 
 
 maximoRec xs =
     case xs of
@@ -56,6 +32,13 @@ maximoRec xs =
                 if b > prevValor
                 then Just b
                 else Just prevValor
+
+
+
+
+
+
+--VALOR MINIMO EN LISTA
 
 minaux xs estado = 
     case xs of 
@@ -71,23 +54,35 @@ minimo xs =
     b1 :: bs ->
         Just (minaux bs b1)
 
-minYMax2 xs =
+
+
+
+
+
+
+
+--VALOR MINIMO Y MAXIMO EN LISTA
+
+minmax2 xs =
     case xs of
     [] -> Nothing
     b1 :: bs -> Just ((minaux bs b1), (maximoAux bs b1))
 
 
+--ANOTHER WAY
 
-
-
-minmaxaux xs mayor menor  =
+minmaxaux xs menor mayor =
     case xs of
     [] -> (menor, mayor)
-    b1 :: bs -> if b1 > mayor
-                case b1 of
-                
-                then minmaxaux bs b1
-                else minmaxaux bs (menor, mayor)
+    b :: bs -> if b > mayor 
+                then minmaxaux bs menor b 
+                else if b < menor then minmaxaux bs b mayor else minmaxaux bs menor mayor  
+
+
+minmax xs =
+    case xs of
+    [] -> Nothing
+    b1 :: bs -> Just (minmaxaux bs b1 b1)  
     
     
 
--- informatica I/ejercicios/Sofia Gabrie/parcial2/src/E.elm
+++ informatica I/ejercicios/Sofia Gabrie/parcial2/src/E.elm
@@ -20,6 +20,23 @@ contarVocales xs =
     contarVocalesaux xs []
 
 
+--ANOTHER WAY
+
+esVocal a = a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u'
+
+contarVocales2 xs =
+    case xs of 
+    [] -> 0
+    b :: bs -> 
+        if esVocal b
+        then 1 + contarVocales2 bs
+        else contarVocales2 bs
+
+
+
+
+
+
 
 
 --Una función llamada eliminarVocales que acepta una lista de caracteres 
@@ -42,6 +59,18 @@ eliminarVocales xs =
 
 
 
+--ANOTHER WAY
+
+eliminarVocales2 xs =
+    case xs of 
+    [] -> []
+    b :: bs ->
+        if not (esVocal b)
+        then b :: eliminarVocales2 bs
+        else eliminarVocales2 bs
+
+
+
 
 
 --3.Defina un tipo algebraico llamado Fracción que se utilizara para representar fracciones de numeros enteros. 
@@ -57,12 +86,19 @@ type Fraccion = Fraccion Int Int
 --Defina una función llamada sumarFracciones que acepta dos valores del tipo Fracción del inciso anterior 
 --y retorna una nueva fracción que corresponde a la suma de las dos fracciones que se dieron como parametros.
 
-
+--MINE prob wrong (returns float)
 sumarFracciones x y =
     case (x,y) of
     (Fraccion n1 d1, Fraccion n2 d2) -> (toFloat (n1*d2 + n2*d1))/(toFloat(d1*d2))
 
 
+--ANOTHER WAY
+sumar1 (Fraccion n1 d1, Fraccion n2 d2) = 
+    Fraccion (n1*d2 + n2*d1) (d1*d2)
 
 
-                
+--ANOTHER WAY
+sumar2 f1 f2 =
+    case (f1,f2) of
+    (Fraccion n1 d1, Fraccion n2 d2) ->
+        Fraccion (n1*d2 + n2*d1) (d1*d2)
